388f21c862540c2dfc59af434ec181e68299fbda
Thomas Nield <thomasnield@live.com>
2020-03-10 18:46:31 -0500

    wip

--- oreilly-introduction-to-regular-expressions/initialize.sh
+++ oreilly-introduction-to-regular-expressions/initialize.sh
@@ -1,3 +1,3 @@
-sudo apt-get update 
-sudo apt-get -y install python 3.3.8
-sudo apt-get -y install python3-pip
+# sudo apt-get update 
+# sudo apt-get -y install python 3.3.8
+# sudo apt-get -y install python3-pip
