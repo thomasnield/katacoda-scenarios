c6f4f6e1c7823b9a5e398c65aaf8009b357d0227
Thomas Nield <thomasnield@live.com>
2020-03-11 15:04:30 -0500

    wip

--- sql-demo/initialize.sh
+++ sql-demo/initialize.sh
@@ -1,4 +1,4 @@
 
 curl -L https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true >> thunderbird_manufacturing.db
 
-apt-get -qq update && apt-get -qq --yes --force-yes install sqlite3 && apt-get install sqlite3-pcre && sqlite3 -column -header -load "/usr/lib/sqlite3/pcre.so" "thunderbird_manufacturing.db" 
+apt-get -qq update && apt-get -qq --yes --force-yes install sqlite3 && apt-get install sqlite3-pcre && sqlite3 -column -header "thunderbird_manufacturing.db" && .load "/usr/lib/sqlite3/pcre.so"
