533e5dcc674ca990f0634a37e8237892394a579e
Thomas Nield <thomasnield@live.com>
2020-03-11 13:30:51 -0500

    wip

--- sql-demo/initialize.sh
+++ sql-demo/initialize.sh
@@ -1,7 +1,7 @@
 wget https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true
 
-sudo apt-get update 
-sudo apt-get -y install sqlite3
+# sudo apt-get update 
+# sudo apt-get -y install sqlite3
 
 sqlite3
 
