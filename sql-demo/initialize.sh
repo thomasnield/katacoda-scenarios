1a758c392eef3bfba3b04b2484bb672025129e58
Thomas Nield <thomasnield@live.com>
2020-03-11 13:33:46 -0500

    wip

--- sql-demo/initialize.sh
+++ sql-demo/initialize.sh
@@ -1,8 +1,3 @@
-wget https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true
-
-# sudo apt-get update 
-# sudo apt-get -y install sqlite3
-
-sqlite3
+curl https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true
 
 attach "thunderbird_manufacturing.db" as thunderbird_manufacturing;
