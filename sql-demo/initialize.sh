c8229ca8958ebde6693aa80dbcf3190711ee1162
Thomas Nield <thomasnield@live.com>
2020-03-11 13:42:14 -0500

    wip

--- sql-demo/initialize.sh
+++ sql-demo/initialize.sh
@@ -1,3 +1,3 @@
-curl -L https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true
+curl -L https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true >> thunderbird_manufacturing.db
 
 attach "thunderbird_manufacturing.db" as thunderbird_manufacturing;
