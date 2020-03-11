df9ff0cd5440cc83253557e31b2c1f2b19126163
Thomas Nield <thomasnield@live.com>
2020-03-10 20:12:59 -0500

    wip

--- oreilly-introduction-to-regular-expressions/initialize.sh
+++ oreilly-introduction-to-regular-expressions/initialize.sh
@@ -3,12 +3,3 @@
 # sudo apt-get -y install python3-pip
 
 python3
-import re 
-def regex_match(regex, str):
-    result = re.fullmatch(pattern=regex, string=str)
-    if result:
-        return "MATCH"
-    else:
-        return "NO MATCH"
-		
-
