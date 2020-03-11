6fd15a0a5b759ab0527a3cc2cbada673f17abb2d
Thomas Nield <thomasnield@live.com>
2020-03-11 15:38:42 -0500

    wip

--- sql-demo/initialize.sh
+++ sql-demo/initialize.sh
@@ -3,12 +3,7 @@ curl -L https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/ma
 
 curl -L https://sqlite.org/2020/sqlite-tools-linux-x86-3310100.zip >> sqlite.zip
 
-# apt-get -qq --yes --force-yes install sqlite3
-
 unzip sqlite.zip 
 
-sudo cp -v sqlite-tools-linux-x86-3210000/sqlite3 /usr/bin/
+apt-get -qq update && apt-get -qq --yes --force-yes install sqlite3 && cp -v sqlite-tools-linux-x86-3310100/sqlite3 /usr/bin/ && apt-get install sqlite3-pcre && ./sqlite3 -column -header "thunderbird_manufacturing.db" && .load "/usr/lib/sqlite3/pcre.so"
 
-apt-get -qq update && apt-get -qq --yes --force-yes install sqlite3 && apt-get install sqlite3-pcre && ./sqlite3 -column -header "thunderbird_manufacturing.db" && .load "/usr/lib/sqlite3/pcre.so"
-
-cp -v sqlite-tools-linux-x86-3310100/sqlite3 /usr/bin/
