bfcc1685d6a35aa179fea4d7b22e1a11ce7bf67f
Thomas Nield <thomasnield@live.com>
2020-03-11 20:39:25 -0500

    wip

--- sql-demo/initialize.sh
+++ sql-demo/initialize.sh
@@ -10,4 +10,4 @@ gcc shell.c sqlite3.c -lpthread -ldl -o sqlite3 &&
 curl -L "https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true" >> thunderbird_manufacturing.db &&
 apt-get -qq --yes --force-yes install sqlite3-pcre && 
 ./sqlite3 -column -header "thunderbird_manufacturing.db" &&
-.width 15 15 15 15 15 15 15 15 15 15 15 15 15
+.width 30 30 30 30 30 30 30 30 30 30 30 30 30
