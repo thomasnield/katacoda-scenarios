af3a083ce74e6dd76cda987020d188c0e866a849
Thomas Nield <thomasnield@live.com>
2020-04-13 18:04:20 -0500

    wip

--- oreilly-sql-01-retrieve-data/initialize.sh
+++ oreilly-sql-01-retrieve-data/initialize.sh
@@ -10,4 +10,6 @@ gcc shell.c sqlite3.c -lpthread -ldl -o sqlite3 &&
 apt-get -qq --yes --force-yes install sqlite3-pcre && 
 mv sqlite3 ../sqlite3 &&
 reset && cd .. && curl -L "https://github.com/thomasnield/oreilly_intermediate_sql_for_data/blob/master/thunderbird_manufacturing.db?raw=true" >> thunderbird_manufacturing.db &&
+rm -rf sqlite-amalgamation-3310100 &&
+rm sqlite.zip && 
 ./sqlite3 -column -header "thunderbird_manufacturing.db"
