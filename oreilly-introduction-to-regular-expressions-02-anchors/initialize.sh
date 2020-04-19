b98891a1039288208526c8d23f599d00921b71f2
Thomas Nield <thomasnield@live.com>
2020-04-18 21:53:07 -0500

    wip

--- oreilly-introduction-to-regular-expressions-02-anchors/initialize.sh
+++ oreilly-introduction-to-regular-expressions-02-anchors/initialize.sh
@@ -1,17 +1,7 @@
-echo "French Macaroons (24 servings)
 
-6 egg whites
-1 cups white sugar
-2 cups of powdered sugar
-2 cups of almond flour
+curl -L https://gist.githubusercontent.com/thomasnield/baf13fa82a70814f51053e0ac1e9147d/raw/9c53ad9fee4b10d7ef9f9ac754defd491a88bec5/french_macroons.txt >> recipe.txt
 
-Beat eggs in bowl and then add sugar.
-
-Let sit for 2 minutes and continue beating" >> recipe.txt
-
-echo  "AS ABQ DAL 12:35 13:45 180
-NK PHX HOU 14:15 16:45 190
-AA MDW JFK 12:45 14:35 180" >> flights.txt
+curl -L https://gist.githubusercontent.com/thomasnield/0b8816d707edf7efe5dc7c20f6e60729/raw/f517f501a0ffb2fb70fe72b3e8e9468f0c8117e8/flights.txt >> flights.txt
 
 
 curl -L  https://gist.githubusercontent.com/thomasnield/95897f86e7495d33173b08f7cfe20a38/raw/62a5ce5596f91fe02cc436cac6b4909e4670bc04/regex_helper.py >> regex_helper.py
---
+++ oreilly-introduction-to-regular-expressions-02-anchors/string_anchors.md
@@ -0,0 +1,12 @@
+The `^` and `$` anchor the start and end of a line respectively. When you are scanning a multiline string or document, you can anchor the start and end of the entire string/document with `\A` and `\Z`. 
+
+To match the first letter in a document, only if it starts the document, run the code below: 
+
+`findall(regex=`\A[A-Za-z]`, file='recipe.txt')`{{execute}}
+
+The document starts with the letter "F" so that is what is returned. If the first character was not alphabetic, no return would come back at all. 
+
+You can lso qualify the end of the document or string with `\Z`. To match the last character in a document, only if it is an alphabetic letter, follow the character range with `\Z`: 
+
+`findall(regex=`[A-Za-z]\Z`, file='recipe.txt')`{{execute}}
+
