16bbf07f72c40d03b3b57f2d6f5374fbb6e9b313
Thomas Nield <thomasnield@live.com>
2020-04-18 20:46:12 -0500

    wip

--- oreilly-introduction-to-regular-expressions-01-literals-ranges/initialize.sh
+++ oreilly-introduction-to-regular-expressions-01-literals-ranges/initialize.sh
@@ -3,32 +3,21 @@
 # sudo apt-get -y install python3-pip
 
 
-echo " 
-French Macaroons (24 servings)
-
-6 egg whites
-1 cups white sugar
-2 cups of powdered sugar
-2 cups of almond flour
-
-Beat eggs in bowl and then add sugar.
-
-Let sit for 2 minutes and continue beating
-" >> recipe.txt
-
-echo  "
-AS ABQ DAL 12:35 13:45 180
-NK PHX HOU 14:15 16:45 190
-AA MDW JFK 12:45 14:35 180
-" >> flights.txt
+echo "
+import re
 
 
-wget https://gist.githubusercontent.com/thomasnield/95897f86e7495d33173b08f7cfe20a38/raw/19309c5f869e65f513c38bbab7893a4da72bfb75/regex_helper.py
+def match(regex, str):
+    result = re.fullmatch(pattern=regex, string=str)
+    if result:
+        return 'MATCH'
+    else:
+        return 'NO MATCH'" >> regex_helper.py
 
 
 python3
 
-from regex_helper import match, find, findall, split
+from regex_helper import match
 
 import os
 os.system('clear')
